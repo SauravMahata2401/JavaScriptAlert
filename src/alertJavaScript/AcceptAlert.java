87d831562369435980880106dc3a0fed4880cf5e
SauravMahata2401 <sauravmahata24@gmail.com>
2019-09-10 00:12:41 +0530

    all code committed

---
+++ src/alertJavaScript/AcceptAlert.java
@@ -0,0 +1,44 @@
+package alertJavaScript;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class AcceptAlert {
+	
+	WebDriver driver;
+	
+	@Test
+	public void CheckAlert(){
+		
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+		driver.findElement(By.name("proceed")).click();
+		
+		/*Alert alert = driver.switchTo().alert();
+		System.out.println(alert.getText());
+		alert.accept();*/
+		
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		
+		System.out.println(alert.getText());
+		alert.accept();
+		
+		driver.quit();
+	}
+
+}
